d9285749164228cab2d40e8127897666f07252c3
JuanLeonI <Juandanielleon22@gmail.com>
2025-04-15 18:34:29 -0700

    s

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $('.title').click(function(){
-      $('.container').addClass('open');
-    });
-    
-    $('.close').click(function(){
-      $('.container').removeClass('open');
-    });
+  $('.title').click(function(){
+    $('.container').addClass('open');
   });
+  
+  $('.close').click(function(){
+    $('.container').removeClass('open');
+  });
+});
